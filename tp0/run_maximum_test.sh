fa4b0dec3fa208a66682257b56c19c160176edce
gabyrobles93 <gabyrobles93@gmail.com>
2018-10-19 23:24:52 -0300

    Add test script in bash.

---
+++ tp0/run_maximum_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+N=1;
+while :; do
+head -c $N </dev/urandom >/tmp/in.bin;
+./tp0 -a encode -i /tmp/in.bin -o /tmp/out.b64;
+./tp0 -a decode -i /tmp/out.b64 -o /tmp/out.bin;
+if diff /tmp/in.bin /tmp/out.bin; then :; else
+echo ERROR: $N;
+break;
+fi
+echo ok: $N;
+N=$((N+1))
+rm -f /tmp/in.bin /tmp/out.b64 /tmp/out.bin
+done
